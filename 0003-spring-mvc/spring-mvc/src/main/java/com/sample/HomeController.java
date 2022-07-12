dfe13cf446aaf1dd9da976705798480d08c93b63
Nemat <A.Nemat@shaparak.com>
2022-07-12 08:39:21 +0430

    add mvc sample

---
+++ 0003-spring-mvc/spring-mvc/src/main/java/com/sample/HomeController.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+
+    @RequestMapping("/home")
+    public String getHome() {
+        return "home";
+    }
+
+
+
+}
