380b0f4581fbf8bd4be5a7e3ba19ba92a4472515
Nemat <A.Nemat@shaparak.com>
2022-08-21 16:51:11 +0430

    screenshots added

--- 0031-spring-boot-sample2/demo/src/main/java/com/example/demo/controller/HomeController.java
+++ 0031-spring-boot-sample2/demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,14 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${player.name}")
+    private String name;
+    @Value("${player.family}")
+    private String family;
+
     @GetMapping("/test")
     public String sayHello() {
         return "Hello, Dolly";
+    }
+
+    @GetMapping("/getinfo")
+    public String getInfo() {
+        return name + " - " + family;
     }
 
 }
